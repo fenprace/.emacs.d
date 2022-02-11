4ebf220a536fbb96c1158bb4bb1f6f68d2651e29
Zhuo FENG <fenprace.i@gmail.com>
2022-02-12 01:03:08 +0800

    add eslint support

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
    '("a7b20039f50e839626f8d6aa96df62afebb56a5bbd1192f557cb2efb5fcfb662" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(eglot treemacs exec-path-from-shell flycheck company undo-tree which-key rainbow-delimiters doom-themes consult marginalia orderless vertico evil-leader use-package))
+   '(add-node-modules-path eglot treemacs exec-path-from-shell flycheck company undo-tree which-key rainbow-delimiters doom-themes consult marginalia orderless vertico evil-leader use-package))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
